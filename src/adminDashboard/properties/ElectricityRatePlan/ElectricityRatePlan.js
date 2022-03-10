660315ecb97adeec0c22f50e58995cea154961ca
Chris Shatrov <cshatrov@datumsoftware.com>
2022-03-09 23:56:24 -0600

    Added save button

--- src/adminDashboard/properties/ElectricityRatePlan/ElectricityRatePlan.js
+++ src/adminDashboard/properties/ElectricityRatePlan/ElectricityRatePlan.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import {
+    Button,
     Collapse,
     CircularProgress,
     TextField,
@@ -9,6 +10,12 @@ import {
     InputLabel,
     Select,
     MenuItem,
+    FormLabel,
+    RadioGroup,
+    Radio,
+    FormControlLabel,
+    OutlinedInput,
+    InputAdornment,
 } from '@material-ui/core'
 import { API_URL_ADMIN } from '../../../constants'
 import FlashOnOutlinedIcon from '@material-ui/icons/FlashOnOutlined'
@@ -17,7 +24,11 @@ import EditOutlinedIcon from '@material-ui/icons/EditOutlined'
 import ExpandLessIcon from '@material-ui/icons/ExpandLessOutlined'
 import './ElectricityRatePlan.css'
 
-const margins = [{ value: '1', label: 'First Option' }]
+const margins = [
+    { value: 'ABSOLUTE', label: 'Absolute' },
+    { value: 'RELATIVE', label: 'Relative' },
+    { value: 'NOTHING', label: 'None' },
+]
 
 const settings = [
     { value: '1', label: 'First Settng' },
@@ -27,46 +38,77 @@ const settings = [
 export default function ElectricityRatePlan(props) {
     const [isLoading, setIsLoading] = useState(false)
     const [planDetailsOpened, setPlanDetailsOpened] = useState(false)
-    const [l1PlanName, setL1PlanName] = useState('L1 Plan Name')
-    const [l2PlanName, setL2PlanName] = useState('L2 Plan Name')
-    const [l1Setting, setL1Setting] = useState('1')
-    const [l2Setting, setL2Setting] = useState('1')
-    const [marginAmount, setMarginAmount] = useState('5')
-    const [margin, setMargin] = useState(1)
+    const [utilityRatePlan, setUtilityRatePlan] = useState(null)
+    const [l1MaginRate, setL1MaginRate] = useState(null)
+    const [l2MaginRate, setL2MaginRate] = useState(null)
+    const [marginAmount, setMarginAmount] = useState(null)
+    const [margin, setMargin] = useState(0.0)
+    const [disabledButton, setDisabledButton] = useState(true)
 
     const toggleInfo = () => {
         setPlanDetailsOpened(!planDetailsOpened)
     }
 
-    const handlePlanNameChange = (value, type) => {
-        if (type === 'l1') {
-            setL1PlanName(value)
-        } else {
-            setL2PlanName(value)
-        }
+    const handlePlanNameChange = (value) => {
+        setUtilityRatePlan(value)
     }
 
     const handleSettingChange = (value, type) => {
         if (type === 'l1') {
-            setL1Setting(value)
+            setL1MaginRate(value)
         } else {
-            setL2Setting(value)
+            setL2MaginRate(value)
         }
-    }
-
-    const handleMarginChange = (value) => {
-        setMargin(value)
     }
 
     const handleMarginAmountChange = (value) => {
         setMarginAmount(value)
     }
 
+    const handleRadioButtonChange = (event) => {
+        const zero = 0
+        if (event.target.value === 'NOTHING') {
+            setMarginAmount(0)
+        }
+        if (event.target.value === 'ABSOLUTE') {
+            setMarginAmount(zero.toFixed(3))
+        }
+        if (event.target.value === 'RELATIVE') {
+            setMarginAmount(zero.toFixed(1))
+        }
+        setMargin(event.target.value)
+    }
+
+    const getMargins = (chargeType) => {
+        if (props.token && props.propertyUUID) {
+            setIsLoading(true)
+            fetch(API_URL_ADMIN + 'admin/electricity-margins/' + chargeType, {
+                method: 'GET',
+                headers: {
+                    Authorization: 'Bearer ' + props.token,
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((res) => res.json())
+                .then(
+                    (result) => {
+                        setIsLoading(false)
+                        console.log('here is margins result', result)
+                    },
+                    (error) => {
+                        setIsLoading(false)
+                    }
+                )
+        }
+    }
+
     const getPlanInfo = () => {
-        if (props.token) {
+        if (props.token && props.propertyUUID) {
             setIsLoading(true)
             fetch(
-                API_URL_ADMIN + 'admin/electricity-rates/' + props.propertyUUID,
+                API_URL_ADMIN +
+                    'admin/property-power-plans/' +
+                    props.propertyUUID,
                 {
                     method: 'GET',
                     headers: {
@@ -88,9 +130,26 @@ export default function ElectricityRatePlan(props) {
         }
     }
 
+    const validateRelative = () => {
+        let showError = false
+        if (parseFloat(marginAmount) < 0.0 || parseFloat(marginAmount) > 50.0) {
+            showError = true
+        }
+        return showError
+    }
+
+    const validateAbsolute = () => {
+        let showError = false
+        if (parseFloat(marginAmount) < 0.0 || parseFloat(marginAmount) > 0.15) {
+            showError = true
+        }
+        return showError
+    }
+
     useEffect(() => {
         getPlanInfo()
-        getPlanInfo()
+        getMargins('L1')
+        getMargins('L2')
     }, [])
 
     return (
@@ -123,25 +182,14 @@ export default function ElectricityRatePlan(props) {
                         ></Divider>
                         <Grid item>
                             <div className="light-grey plan-badge">
-                                <div className="badgeBox">
-                                    <span className="badgeText">
-                                        {l1PlanName}
-                                    </span>
-                                </div>
-                            </div>
-                        </Grid>
-                        <Divider
-                            orientation="vertical"
-                            variant="middle"
-                            flexItem
-                        ></Divider>
-                        <Grid item>
-                            <div className="light-grey plan-badge">
-                                <div className="badgeBox">
-                                    <span className="badgeText">
-                                        {l2PlanName}
-                                    </span>
-                                </div>
+                                {utilityRatePlan && (
+                                    <div className="badgeBox">
+                                        <span className="badgeText">
+                                            {utilityRatePlan}
+                                        </span>
+                                    </div>
+                                )}
+                                {!utilityRatePlan && <div>No Plan</div>}
                             </div>
                         </Grid>
                         <Divider
@@ -190,31 +238,13 @@ export default function ElectricityRatePlan(props) {
                             <TextField
                                 fullWidth
                                 className="editableField"
-                                id="l1planName"
-                                label="L1 Plan Name"
+                                id="planName"
+                                label="Utility Rate Plan"
                                 variant="outlined"
-                                value={l1PlanName}
+                                value={utilityRatePlan}
                                 onChange={(e) =>
-                                    handlePlanNameChange(e.target.value, 'l1')
+                                    handlePlanNameChange(e.target.value)
                                 }
-                                // onBlur={() => saveLCUInfo()}
-                                InputProps={{
-                                    endAdornment: <EditOutlinedIcon />,
-                                }}
-                            />
-                        </Grid>
-                        <Grid item lg={3} md={6} s={12} xs={12}>
-                            <TextField
-                                fullWidth
-                                className="editableField"
-                                id="l2planName"
-                                label="L2 Plan Name"
-                                variant="outlined"
-                                value={l2PlanName}
-                                onChange={(e) =>
-                                    handlePlanNameChange(e.target.value, 'l2')
-                                }
-                                // onBlur={() => saveLCUInfo()}
                                 InputProps={{
                                     endAdornment: <EditOutlinedIcon />,
                                 }}
@@ -226,21 +256,20 @@ export default function ElectricityRatePlan(props) {
                                 className="editableFieldSelectContainer"
                             >
                                 <InputLabel id="l1Setting">
-                                    L1 Setting
+                                    L1 Margin Rate Plan
                                 </InputLabel>
                                 <Select
                                     labelId="l1Setting"
                                     variant="outlined"
                                     id="l1Setting"
-                                    value={l1Setting}
+                                    value={l1MaginRate}
                                     onChange={(e) =>
                                         handleSettingChange(
                                             e.target.value,
                                             'l1'
                                         )
                                     }
-                                    // onBlur={() => saveLCUInfo()}
-                                    label="L1 Setting"
+                                    label="L1 Margin Rate Plan"
                                 >
                                     {settings?.map((option) => {
                                         return (
@@ -261,21 +290,20 @@ export default function ElectricityRatePlan(props) {
                                 className="editableFieldSelectContainer"
                             >
                                 <InputLabel id="l2Setting">
-                                    L2 Setting
+                                    L2 Margin Rate Plan
                                 </InputLabel>
                                 <Select
                                     labelId="l2Setting"
                                     variant="outlined"
                                     id="l2Setting"
-                                    value={l2Setting}
+                                    value={l2MaginRate}
                                     onChange={(e) =>
                                         handleSettingChange(
                                             e.target.value,
                                             'l2'
                                         )
                                     }
-                                    // onBlur={() => saveLCUInfo()}
-                                    label="L2 Setting"
+                                    label="L2 Margin Rate Plan"
                                 >
                                     {settings?.map((option) => {
                                         return (
@@ -290,52 +318,115 @@ export default function ElectricityRatePlan(props) {
                                 </Select>
                             </FormControl>
                         </Grid>
-                        <Grid item lg={3} md={6} s={12} xs={12}>
-                            <FormControl
-                                fullWidth
-                                className="editableFieldSelectContainer"
-                            >
-                                <InputLabel id="plan">Margin</InputLabel>
-                                <Select
-                                    labelId="margin"
-                                    variant="outlined"
-                                    id="margin"
+                        <Grid item lg={12} md={12} s={12} xs={12}>
+                            <FormControl className="radioButtonGroup">
+                                <FormLabel id="demo-row-radio-buttons-group-label">
+                                    Margin Type
+                                </FormLabel>
+                                <RadioGroup
                                     value={margin}
-                                    onChange={(e) =>
-                                        handleMarginChange(e.target.value)
-                                    }
-                                    // onBlur={() => saveLCUInfo()}
-                                    label="Margin"
+                                    row
+                                    aria-labelledby="demo-row-radio-buttons-group-label"
+                                    name="row-radio-buttons-group"
                                 >
                                     {margins?.map((option) => {
                                         return (
-                                            <MenuItem
+                                            <FormControlLabel
                                                 key={option.value}
                                                 value={option.value}
-                                            >
-                                                {option.label ?? option.value}
-                                            </MenuItem>
+                                                label={option.label}
+                                                onChange={
+                                                    handleRadioButtonChange
+                                                }
+                                                control={
+                                                    <Radio
+                                                        color="#12BFA2 !important"
+                                                        sx={{
+                                                            color:
+                                                                '#12BFA2 !important',
+                                                            '&.Mui-checked': {
+                                                                color:
+                                                                    '#12BFA2 !important',
+                                                            },
+                                                        }}
+                                                    />
+                                                }
+                                            />
                                         )
                                     })}
-                                </Select>
+                                </RadioGroup>
                             </FormControl>
                         </Grid>
-                        <Grid item lg={3} md={6} s={12} xs={12}>
-                            <TextField
-                                fullWidth
-                                className="editableField"
-                                id="marginAmount"
-                                label="Amount of Margin"
+                        {margin === 'ABSOLUTE' && (
+                            <Grid item lg={3} md={6} s={12} xs={12}>
+                                <FormControl
+                                    fullWidth
+                                    sx={{ m: 1 }}
+                                    className="editableFieldSelectContainer"
+                                >
+                                    <InputLabel htmlFor="outlined-adornment-amount">
+                                        Margin
+                                    </InputLabel>
+                                    <OutlinedInput
+                                        id="outlined-adornment-amount"
+                                        value={marginAmount}
+                                        type="number"
+                                        error={validateAbsolute() === true}
+                                        helperText="Must be between 0.000 and 0.150"
+                                        onChange={(e) =>
+                                            handleMarginAmountChange(
+                                                e.target.value
+                                            )
+                                        }
+                                        startAdornment={
+                                            <InputAdornment position="start">
+                                                $
+                                            </InputAdornment>
+                                        }
+                                        label="Amount"
+                                    />
+                                </FormControl>
+                            </Grid>
+                        )}
+                        {margin === 'RELATIVE' && (
+                            <Grid item lg={3} md={6} s={12} xs={12}>
+                                <FormControl
+                                    fullWidth
+                                    sx={{ m: 1 }}
+                                    className="editableFieldSelectContainer"
+                                >
+                                    <InputLabel htmlFor="outlined-adornment-amount">
+                                        Margin
+                                    </InputLabel>
+                                    <OutlinedInput
+                                        id="outlined-adornment-amount"
+                                        type="number"
+                                        error={validateRelative() === true}
+                                        helperText="Must be between 0.0 and 50.0"
+                                        value={marginAmount}
+                                        onChange={(e) =>
+                                            handleMarginAmountChange(
+                                                e.target.value
+                                            )
+                                        }
+                                        startAdornment={
+                                            <InputAdornment position="start">
+                                                %
+                                            </InputAdornment>
+                                        }
+                                        label="Amount"
+                                    />
+                                </FormControl>
+                            </Grid>
+                        )}
+                        <Grid item xs={12}>
+                            <Button
+                                className="savePlanButton"
                                 variant="outlined"
-                                value={marginAmount}
-                                onChange={(e) =>
-                                    handleMarginAmountChange(e.target.value)
-                                }
-                                // onBlur={() => saveLCUInfo()}
-                                InputProps={{
-                                    endAdornment: <EditOutlinedIcon />,
-                                }}
-                            />
+                                disabled={disabledButton}
+                            >
+                                Save
+                            </Button>
                         </Grid>
                     </Grid>
                 </Collapse>
